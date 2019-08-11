c477fb8e7e05a2e139637cdbae1e295971b106e7
wakame-tech <kamata1919@gmail.com>
2019-08-11 16:45:22 +0900

    [Add] step4; ファイル転送ができない

--- 01/bootload/main.c
+++ 01/bootload/main.c
@@ -1,5 +1,6 @@
 #include "defines.h"
 #include "serial.h"
+#include "xmodem.h"
 #include "lib.h"
 
 static int init(void) {
@@ -15,39 +16,66 @@ static int init(void) {
   return 0;
 }
 
-int global_data = 0x10;
-int global_bss;
-static int static_data = 0x20;
-static int static_bss;
+static int dump(char * buf, long size) {
+  long i;
 
-static void printval(void) {
-  puts("global_data = "); putxval(global_data, 0);
+  if(size < 0) {
+    puts("no data.\n");
+    return -1;
+  }
+
+  for(i = 0; i < size; i++) {
+    putxval(buf[i], 2);
+
+    if((i & 0xf) == 15) {
+      puts("\n");
+    } else {
+      if(i & 0xf == 7) puts(" ");
+      puts(" ");
+    }
+  }
   puts("\n");
-  puts("global_bss = "); putxval(global_bss, 0);
-  puts("\n");
-  puts("static_data = "); putxval(static_data, 0);
-  puts("\n");
-  puts("static_bss = "); putxval(static_bss, 0);
-  puts("\n");
+  return 0;
+}
+
+static void wait() {
+  volatile long i;
+  for(i = 0; i < 300000; i++)
+    ;
 }
 
 int main (void) {
-  serial_init(SERIAL_DEFAULT_DEVICE);
+  static char buf[16];
+  static long size = -1;
+  static unsigned char * loadbuf = NULL;
+  extern int buffer_start; /* define in linker script */
 
   init();
 
-  puts("Hello World!\n");
+  puts("kzload (lozos boot loader) started.\n");
 
-  printval();
-  puts("overwrite variables");
-  global_data = 0x20;
-  global_bss = 0x30;
-  static_data = 0x40;
-  static_bss = 0x50;
-  printval();
+  while(1) {
+    puts("kzload> "); /* prompt */
+    gets(buf);
 
-  while (1)
-    ;
+    if(!strcmp(buf, "load")) {
+      loadbuf = (char *)(&buffer_start);
+      size = xmodem_recv(loadbuf);
+      wait();
+      if(size < 0) {
+        puts("\nXMODEM receive error!\n");
+      } else {
+        puts("\nXMODEM receive succeeded.\n");
+      }
+    } else if(!strcmp(buf, "dump")) {
+      puts("size: ");
+      putxval(size, 0);
+      puts("\n");
+      dump(loadbuf, size);
+    } else {
+      puts("unknown.\n");
+    }
+  }
 
   return 0;
 }
